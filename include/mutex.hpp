bed8c40f7121afaef05777e561b52b908c12cfab
xujj25 <xujj25@foxmail.com>
2018-04-17 22:32:58 +0800

    delete复制函数

--- include/mutex.hpp
+++ include/mutex.hpp
@@ -21,6 +21,17 @@ namespace xjj {
         Mutex();
 
         /*!
+         * @brief 拷贝构造函数，设为delete，阻止拷贝
+         */
+        Mutex(const Mutex&) = delete;
+
+        /*!
+         * @brief 赋值操作，设为delete，阻止赋值
+         * @return Mutex& 
+         */
+        Mutex& operator=(const Mutex&) = delete;
+
+        /*!
          * @brief 析构函数
          */
         ~Mutex();
